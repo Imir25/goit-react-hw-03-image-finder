71582101053e6ac3f63bce8f3b2655a95c75518b
Imir25 <anitra26121998@gmail.com>
2023-11-26 02:04:00 +0300

    fixed image-finder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,47 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import ImageGallery from './ImageGallery';
-import Button from './Button';
-import Loader from './Loader';
+import React, { Component } from "react";
+import ImageGallery from "./ImageGallery/ImageGallery";
+import Modal from "./Modal/Modal";
+import Searchbar from "./Searchbar/Searchbar";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      images: [],
-      loading: false,
-      page: 1,
-    };
-
-    this.apiKey = '39801546-c4bb34864e6abc7825d1e4868'; 
+  state = {
+    inputValue: '',
+    modalImg: '',
+    showModal: false,
+    page: 1,
   }
 
-  componentDidMount() {
-   
-    this.loadMoreImages();
+  getInputValue = handleValue => {
+    this.setState({ inputValue: handleValue, page: 1 })
   }
 
-  loadMoreImages = () => {
-    const { page, images } = this.state;
-    this.setState({ loading: true });
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }))
+  }
 
-    
-    axios
-      .get(`https://pixabay.com/api/?key=${this.apiKey}&q=nature&per_page=5&page=${page}`)
-      .then((response) => {
-        const newImages = response.data.hits;
+  getLargeImg = url => {
+    this.toggleModal();
+    this.setState({ modalImg: url });
+  }
 
-        
-        this.setState((prevState) => ({
-          images: [...prevState.images, ...newImages],
-          loading: false,
-          page: prevState.page + 1,
-        }));
-      })
-      .catch((error) => {
-        console.error('Error fetching images:', error);
-        this.setState({ loading: false });
-      });
+  loadMoreBtn = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
   };
 
+
   render() {
-    const { images, loading } = this.state;
+    const { modalImg, showModal ,page} = this.state;
 
     return (
-      <div className="App">
-        <h1>Image Gallery App</h1>
-        <ImageGallery images={images} />
-        {loading && <Loader />}
-        {images.length > 0 && !loading && <Button onClick={this.loadMoreImages} />}
-      </div>
-    );
+      <>
+        <Searchbar getInputValue={this.getInputValue}/>
+        <ImageGallery inputValue={this.state.inputValue} onClick={this.getLargeImg} loadMoreBtn={this.loadMoreBtn} page={ page}/>
+        {showModal && <Modal url={modalImg} onClose={this.toggleModal} />}
+      </>
+    )
   }
 }
 
