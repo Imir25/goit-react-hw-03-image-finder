9f204e7c853dbbe5135c5c35e1a7925b45cdc3d2
Imir25 <anitra26121998@gmail.com>
2023-11-30 16:32:17 +0300

    fixed modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -7,14 +7,12 @@ const modalRoot = document.querySelector('#modal-root');
 
 class Modal extends Component {
     componentDidMount() {
-        console.log('Modal componentDidMount');
         document.addEventListener('keydown', this.handleKeyDown);
-    };
+    }
 
     componentWillUnmount() {
-        console.log('Modal componentWillUnmount');
         document.removeEventListener('keydown', this.handleKeyDown);
-    };
+    }
 
     handleKeyDown = (e) => {
         if (e.code === 'Escape') {
@@ -23,8 +21,7 @@ class Modal extends Component {
     };
 
     handleBackdropClick = (e) => {
-        console.log('Click on Backdrop');
-        if (e.target === !e.currentTarget) {
+        if (e.target !== e.currentTarget) {
             this.props.closeModal();
         }
     };
@@ -34,18 +31,17 @@ class Modal extends Component {
         return createPortal(
             <div className={css.overlay} onClick={this.handleBackdropClick}>
                 <div className={css.modal}>
-                    <img src={largeImageURL} alt={tags}/>
+                    <img src={largeImageURL} alt={tags} />
                 </div>
             </div>,
             modalRoot
         );
     }
-};
+}
 
 Modal.propTypes = {
     tags: PropTypes.string.isRequired,
     largeImageURL: PropTypes.string.isRequired,
 };
-
 
 export default Modal;
